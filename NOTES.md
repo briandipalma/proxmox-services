3c9265f2de31b24c999155e008220b11c70db9cc
Brian Di Palma <briandipalma [at] outlook.com>
2021-07-27 08:01:13 +0100

    feat(nzbhydra): Delete Terraform scripts, replaced with Prowlarr

--- NOTES.md
+++ NOTES.md
@@ -7,10 +7,6 @@ Set directory UID/GID: `chown -R 100110:100117 jellyfin/`
 
 Set directory UID/GID: `chown -R 100110:101000 sonarr/`
 
-# NZBHydra
-
-Set directory UID/GID: `chown -R 101000:100100 nzbhydra/`
-
 # Prowlarr
 
 Set directory UID/GID: `chown -R 101000:100100 prowlarr/`
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -45,11 +45,6 @@ variable "radarr_lxcid" {
   default = 503
 }
 
-variable "nzbhydra_lxcid" {
-  type = number
-  default = 504
-}
-
 variable "jackett_lxcid" {
   type = number
   default = 505
@@ -103,11 +98,6 @@ variable "sonarr_mac" {
 variable "radarr_mac" {
   type = string
   default = "36:67:C0:9C:48:9C"
-}
-
-variable "nzbhydra_mac" {
-  type = string
-  default = "06:82:C0:36:D1:FB"
 }
 
 variable "jackett_mac" {
