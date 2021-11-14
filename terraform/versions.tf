9bce7c8520e2a3d8bfb05fb4578bf8ce21984c6a
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-11-14 22:34:47 +0000

    refactor(terraform): Move state to my-data

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -1,7 +1,11 @@
 terraform {
+  backend "local" {
+    path = "../../my-data/terraform/terraform.tfstate"
+  }
+
   required_providers {
     proxmox = {
-      source = "telmate/proxmox"
+      source  = "telmate/proxmox"
       version = "2.9.0"
     }
   }
