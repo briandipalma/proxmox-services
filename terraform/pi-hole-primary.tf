497b8eefd0e264926f1b273943aa521f5b3e4277
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2021-12-24 09:10:42 +0000

    fix(pi-hole): Move to LTS to maintain OS updates

--- terraform/pi-hole-primary.tf
+++ terraform/pi-hole-primary.tf
@@ -1,14 +1,14 @@
 resource "proxmox_lxc" "pi-hole-primary" {
-  target_node  = "pve"
-  hostname     = "pi-hole-primary"
-  ostemplate   = "local:vztmpl/ubuntu-20.10-standard_20.10-1_amd64.tar.gz"
-  unprivileged = true
-  ostype = "ubuntu"
+  target_node     = "pve"
+  hostname        = "pi-hole-primary"
+  ostemplate      = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  unprivileged    = true
+  ostype          = "ubuntu"
   ssh_public_keys = file(var.pub_ssh_key)
-  start = true
-  onboot = true
-  vmid = var.pi-hole-primary_lxcid
-  memory = 1024
+  start           = true
+  onboot          = true
+  vmid            = var.pi-hole-primary_lxcid
+  memory          = 1024
 
   // Terraform will crash without rootfs defined
   rootfs {
