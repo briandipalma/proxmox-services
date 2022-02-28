877dd7498b71007474194289a5161df05f8c9c9c
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2022-02-28 11:18:52 +0000

    fix: Increase Qbittorrent memory

--- terraform/qbittorrent.tf
+++ terraform/qbittorrent.tf
@@ -8,7 +8,7 @@ resource "proxmox_lxc" "qbittorrent" {
   start           = true
   onboot          = true
   vmid            = var.qbittorrent_lxcid
-  memory          = 5120
+  memory          = 8196
 
   rootfs {
     storage = "local-lvm"
