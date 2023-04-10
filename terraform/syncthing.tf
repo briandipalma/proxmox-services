bb1439c69ad004adec32550a7079849cd6dbb33d
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2023-04-10 09:00:16 +0100

    feat(syncthing): Add pictures folder

--- terraform/syncthing.tf
+++ terraform/syncthing.tf
@@ -1,10 +1,13 @@
 resource "proxmox_lxc" "syncthing" {
   target_node     = "pve"
   hostname        = "syncthing"
-  ostemplate      = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  ostemplate      = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
   unprivileged    = true
   ostype          = "ubuntu"
-  ssh_public_keys = file(var.pub_ssh_key)
+  ssh_public_keys = <<-EOT
+   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA6iDv3SIqB+4ycb9iuDNbxZ5Koz87LKTZG/QXuwBZgN brian@pop-os-2021-03-20
+	  ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0hWDvdigGG+CV1P73mdjz6b7fXBGmfhg962pDEgT/9 brian@samsung_s20_fe-2022-08-19
+  EOT
   start           = true
   onboot          = true
   vmid            = var.syncthing_lxcid
@@ -52,6 +55,15 @@ resource "proxmox_lxc" "syncthing" {
     volume  = "/mnt/storage/media/music"
   }
 
+  mountpoint {
+    mp      = "/mnt/storage/media/pictures"
+    size    = "8G"
+    slot    = 4
+    key     = "4"
+    storage = "/mnt/storage/media/pictures"
+    volume  = "/mnt/storage/media/pictures"
+  }
+
   network {
     name   = "eth0"
     bridge = "vmbr0"
@@ -65,7 +77,8 @@ resource "proxmox_lxc" "syncthing" {
       mountpoint[0].storage,
       mountpoint[1].storage,
       mountpoint[2].storage,
-      mountpoint[3].storage
+      mountpoint[3].storage,
+      mountpoint[4].storage
     ]
   }
 }
