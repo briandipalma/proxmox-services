93cda737b1a6b14891e76c83166dcedaf7afb38b
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-11-02 22:26:43 +0000

    chore(jellyfin): Updated to latest image

--- terraform/jellyfin.tf
+++ terraform/jellyfin.tf
@@ -1,10 +1,13 @@
 resource "proxmox_lxc" "jellyfin" {
   target_node     = "pve"
   hostname        = "jellyfin"
-  ostemplate      = "local:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
+  ostemplate      = "local:vztmpl/ubuntu-22.04-standard_22.04-1_amd64.tar.zst"
   unprivileged    = true
   ostype          = "ubuntu"
-  ssh_public_keys = file(var.pub_ssh_key)
+  ssh_public_keys = <<-EOT
+   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0hWDvdigGG+CV1P73mdjz6b7fXBGmfhg962pDEgT/9 brian@samsung_s20_fe-2022-08-19
+   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK7AORfN9dPrfHvH5KT0DFUQBmqdkGVpb1RTgs1TN2kN brian@infinitybook_pro14-2024-11-21
+  EOT
   start           = true
   onboot          = true
   vmid            = var.jellyfin_lxcid
@@ -14,7 +17,7 @@ resource "proxmox_lxc" "jellyfin" {
   // Terraform will crash without rootfs defined
   rootfs {
     storage = "local-lvm"
-    size    = "4G"
+    size    = "6G"
   }
 
   mountpoint {
