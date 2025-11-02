6f271f66bf070d301fcc430ecabc4ebfc7d293a6
Brian Di Palma <1597820+briandipalma@users.noreply.github.com>
2025-11-02 22:27:14 +0000

    chore(immich): Updated public ssh keys

--- terraform/immich.tf
+++ terraform/immich.tf
@@ -5,8 +5,8 @@ resource "proxmox_lxc" "immich" {
   unprivileged    = true
   ostype          = "ubuntu"
   ssh_public_keys = <<-EOT
+   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA6iDv3SIqB+4ycb9iuDNbxZ5Koz87LKTZG/QXuwBZgN brian@pop-os-2021-03-20
    ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK0hWDvdigGG+CV1P73mdjz6b7fXBGmfhg962pDEgT/9 brian@samsung_s20_fe-2022-08-19
-   ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIK7AORfN9dPrfHvH5KT0DFUQBmqdkGVpb1RTgs1TN2kN brian@infinitybook_pro14-2024-11-21
   EOT
   start           = true
   onboot          = true
